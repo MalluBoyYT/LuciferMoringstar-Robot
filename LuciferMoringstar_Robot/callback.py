08c45f1c48a5f02648ae5724c3e982695f24b3bb
MalluBoyYT <104717297+MalluBoyYT@users.noreply.github.com>
2022-05-05 08:05:47 +0530

    Update callback.py

--- LuciferMoringstar_Robot/callback.py
+++ LuciferMoringstar_Robot/callback.py
@@ -267,8 +267,8 @@ async def cb_handler(client: lucifermoringstar_robot, query):
                  InlineKeyboardButton("ℹ️ Help", callback_data="help"),
                  InlineKeyboardButton("😎 About", callback_data="about") 
                  ],[
-                 InlineKeyboardButton("🗳 Deploy", url="https://youtu.be/FCU_XJmyG_U"),
-                 InlineKeyboardButton("🤖 Update", url="https://t.me/Mo_Tech_Group")
+                 InlineKeyboardButton("🗳 Deploy", url="https://t.me/MalluHubTG"),
+                 InlineKeyboardButton("🤖 Update", url="https://t.me/MalluHubTG")
                  ]]
             else:
                 buttons = [[
@@ -277,8 +277,8 @@ async def cb_handler(client: lucifermoringstar_robot, query):
                  InlineKeyboardButton("ℹ️ Help", callback_data="bot_owner"),
                  InlineKeyboardButton("😎 About", callback_data="about") 
                  ],[
-                 InlineKeyboardButton("🗳 Deploy", url="https://youtu.be/FCU_XJmyG_U"),
-                 InlineKeyboardButton("🤖 Update", url="https://t.me/Mo_Tech_Group")
+                 InlineKeyboardButton("🗳 Join", url="https://t.me/MalluHubTG"),
+                 InlineKeyboardButton("🤖 Update", url="https://t.me/MalluHubTG")
                  ]]               
             await query.message.edit(text=START_MSG.format(mention=query.from_user.mention, bot_name=bot_info.BOT_NAME, bot_username=bot_info.BOT_USERNAME), reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
 
